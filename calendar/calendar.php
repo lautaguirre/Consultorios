bb07059233dbb351a99fc05c86fe7535673c8c24
lautaguirre <32684961+lautaguirre@users.noreply.github.com>
2017-11-02 15:54:32 -0300

    calendar set up

--- calendar/calendar.php
+++ calendar/calendar.php
@@ -1,81 +1,3 @@
-<?php
-    session_start();
-    $logindni=$loginpass='';
-    $validation=true;
-
-    //DB connection
-    require '../pages/connection.php';
-
-    if(isset($_POST['changepasssubmit'])){
-        //Validation
-        $changepass=sanitizestring('changepass');
-        $changepassdni=sanitizeint('changepassdni');
-        $changepass2=sanitizestring('changepass2');
-
-        validateint($changepassdni);
-
-        if($validation){
-            if($changepass==$changepass2 and $changepassdni==$_SESSION['logged']){
-                $hashednewpass=password_hash($changepass, PASSWORD_DEFAULT);
-                $sql='UPDATE clientes SET password="'.$hashednewpass.'" WHERE dni='.$changepassdni;
-                if(mysqli_query($conn,$sql)){
-                    echo 'Contraseña actualizada<BR>';
-                }
-            }
-        }
-    }
-    
-    if(!isset($_SESSION['logged'])){
-        if($_SERVER['REQUEST_METHOD']=='POST'){
-
-            //Validation 
-            $loginpass=sanitizestring('loginpass');
-            $logindni=sanitizeint('logindni');
-
-            validateint($logindni);
-
-            //Check user query
-            if($validation){
-                $sql='SELECT password,authorization FROM clientes WHERE dni='.$logindni;
-                $result=mysqli_query($conn,$sql);
-                if(mysqli_num_rows($result)==1){
-                    $row=mysqli_fetch_assoc($result);
-                    if(password_verify($loginpass,$row['password'])){
-                        if($row['authorization']=='si'){
-                            $_SESSION['logged']=$logindni;
-                        }
-                        if($logindni==38333166){
-                            $_SESSION['admin']=true;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-
-    //Sanitize
-    function sanitizeint($inttosanitize){
-        global $conn;
-        return mysqli_real_escape_string($conn,(strip_tags($_POST[$inttosanitize])));
-    }
-
-    function sanitizestring($stringtosanitize){
-        global $conn;
-        return mysqli_real_escape_string($conn,(strip_tags($_POST[$stringtosanitize])));
-    }
-
-    //Validate
-    function validateint($inttovalidate){
-        if (!filter_var($inttovalidate, FILTER_VALIDATE_INT)) {
-            echo 'Error al insertar DNI o telefono.<br>';
-            global $validation;
-            $validation=false;
-        }
-    }
-    
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -87,9 +9,6 @@
         <script src='locale/es.js'></script>
         <script>
             $(document).ready(function() {
-                var events=[{title  : 'GoToGoogle',start  : '2018-01-01',url: 'http://google.com/'},{title  : 'event2',start  : '2018-01-05',end    : '2018-01-07'}];
-                var event3={title  : 'event3',start  : '2018-01-09T12:30:00',end  : '2018-01-09T17:30:00',allDay : false};
-                events.push(event3);
                 $('#calendar').fullCalendar({
                     minTime:"09:00:00",
                     maxTime:"20:00:00",
@@ -132,8 +51,8 @@
                             return false;
                         }
                     },
-                    events: events
-                    /*[
+                    events:
+                    [
                         {
                             title  : 'GoToGoogle',
                             start  : '2018-01-01',
@@ -150,7 +69,7 @@
                             end  : '2018-01-09T17:30:00',
                             allDay : false // will make the time show
                         }
-                    ]*/
+                    ]
                 });
 
             });
@@ -161,20 +80,6 @@
     </head>
 
     <body>
-        <?php
-        //Check if logged for logout button
-		if(isset($_SESSION['logged'])){
-		echo '<div class="container">
-			<div class="header2">
-				<ul class="headerlist">
-					<li>
-						<A class="btn" HREF = logout.php>Cerrar sesion</A>
-					</li>
-				</ul>
-			</div>
-		</div>';
-		}
-		?>
         <script src="../templates/header.js"></script>
         <div class="content">
 			<div class="container">  
