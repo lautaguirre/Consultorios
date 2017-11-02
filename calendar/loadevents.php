ccca7aea550b4a4ec4d2df792e70defae2e7bf3e
lautaguirre <32684961+lautaguirre@users.noreply.github.com>
2017-11-02 17:11:21 -0300

    trying loadevents

--- calendar/loadevents.php
+++ calendar/loadevents.php
@@ -1,3 +1,23 @@
 <?php
-    
+    if($_POST['moment']=='onload'){
+        $arr=[];
+        $Object = new stdClass();
+        /*$events=[
+            {
+                title  : 'GoToGoogle',
+                start  : '2018-01-01',
+            },
+            {
+                title  : 'event2',
+                start  : '2018-01-05',
+                end    : '2018-01-07'
+            },
+            {
+                title  : 'event3',
+                start  : '2018-01-09T12:30:00',
+                end  : '2018-01-09T17:30:00',
+                allDay : false // will make the time show
+            }
+        ]*/
+    }
 ?>
