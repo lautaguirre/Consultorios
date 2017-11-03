adf6471e9a0daf77111afa7c3fe6877c9ef097d5
lautaguirre <32684961+lautaguirre@users.noreply.github.com>
2017-11-03 14:44:05 -0300

    json events working

--- calendar/loadevents.php
+++ calendar/loadevents.php
@@ -1,7 +1,32 @@
 <?php
     if($_POST['moment']=='onload'){
 
-        $obj=(object)[
+        $servername='localhost';
+        $username='lauta';
+        $password='password';
+        $db='testdb';
+        
+        $conn= mysqli_connect($servername,$username,$password,$db);
+
+        $arr=array();
+
+        $sql='SELECT title,start,end FROM reservas';
+        $result=mysqli_query($conn,$sql);
+        if(mysqli_num_rows($result)>0){
+            while($row=mysqli_fetch_assoc($result)){
+                $obj=(object)[
+                    'title'=>$row['title'],
+                    'start'=>$row['start'],
+                    'end'=>$row['end'],
+                ];
+                array_push($arr,$obj);
+            }
+            $json=json_encode($arr);
+            
+            echo $json;
+        }
+
+        /*$obj=(object)[
             'title'=>'event3',
             'start'=>'2018-01-09T12:30:00',
             'end'=>'2018-01-09T17:30:00',
@@ -11,15 +36,7 @@
             'title'=>'event2',
             'start'=>'2018-01-05',
             'end'=>'2018-01-07',
-        ];
-
-        $arr=array();
-
-        array_push($arr,$obj,$obj2);
-
-        $json=json_encode($arr);
-
-        echo $json;
+        ];*/
 
         /*$events=[
             {
