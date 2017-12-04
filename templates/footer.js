ff13fb17d68c59f109735d5860db8fc468530530
lautaguirre <32684961+lautaguirre@users.noreply.github.com>
2017-12-04 16:05:22 -0300

    old design deleted

--- templates/footer.js
+++
@@ -1,8 +0,0 @@
-document.write(`<div class="footer">
-<div class="container">
-    <ul class='list-inline'>
-        <li><img height="60px" width="125px" src="../images/vmlogo.png"></li>
-        <li style='position:relative;bottom:23px;'><H5 id='webemail'></H5></li>
-    </ul>
-</div>
-</div>`);
